9d73ceeb73fc156df08ededbe7cc6fd74d800c32
Burak Can Mutlu <brkcnmtl034@gmail.com>
2025-05-17 11:38:19 +0300

    interface created

---
+++ main.py
@@ -0,0 +1,24 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Secret Notes")
+window.minsize(width=700, height=600)
+
+title_message_label = tkinter.Label(window, text="Enter Your Title")
+title_message_label.pack()
+title_message_entry = tkinter.Entry(window)
+title_message_entry.pack()
+secret_message_label_1= tkinter.Label(window, text="Enter Your Secret Message")
+secret_message_label_1.pack()
+secret_message_text= tkinter.Text(window, height=20, width=50)
+secret_message_text.pack()
+key_message_label = tkinter.Label(window, text="Enter Master Key")
+key_message_label.pack()
+key_message_entry = tkinter.Entry(window)
+key_message_entry.pack()
+save_encrypted_message_button = tkinter.Button(window, text="Save & Encrypt",width=12,height=0)
+save_encrypted_message_button.pack()
+decrypt_button = tkinter.Button(window, text="Decrypt",width=12,height=0)
+decrypt_button.pack()
+
+window.mainloop()

c23e5d492088b774b823f9f3d09cca1a805f5c72
Burak Can Mutlu <brkcnmtl034@gmail.com>
2025-05-18 13:03:55 +0300

    Modified

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import base64
 import tkinter
 import os
-from tkinter import PhotoImage
+from tkinter import PhotoImage, Button, END
 from tkinter import messagebox
 from cryptography.fernet import Fernet
 
@@ -11,86 +11,69 @@ window.title("Secret Notes")
 window.minsize(width=700, height=600)
 
 
+photo= tkinter.PhotoImage(file="top-secret_10201555.png")
+photo_button=Button(image=photo)
+photo_button.pack()
 
-image= tkinter.PhotoImage(file="top-secret_10201555.png")
-image_label= tkinter.Label(window, image=image)
-image_label.pack()
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
+def decode(key, enc):
+    dec = []
+    enc = base64.urlsafe_b64decode(enc).decode()
+    for i in range(len(enc)):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return "".join(dec)
+
+
+
+
 
 
 
 
 
 def encrypt_message_create_desktop():
-    message=title_message_entry.get()
-    dosya_yolu = os.path.join(os.path.expanduser("~"), "Desktop",message+".txt")
+    title_message=title_message_entry.get()
+    key_message=key_message_entry.get()
     secret_message=secret_message_text.get(1.0, tkinter.END)
-    key_input= key_message_entry.get()
-    key_bytes=key_input.encode()
-    key_bytes=key_bytes.ljust(32)[:32]
-    key=base64.urlsafe_b64encode(key_bytes)
-    with open(dosya_yolu, "w") as dosya:
-        dosya.write(secret_message)
-        dosya.write(str(key))
 
-    fernet=Fernet(key)
-    encrypted_message=fernet.encrypt(secret_message.encode())
-    decrypt_message=fernet.decrypt(encrypted_message).decode()
-    tkinter.messagebox.showinfo("Secret", "Secret Notes Created")
-    print(decrypt_message)
-
-
-
-def decrypt_message():
-    pass
-    def decrypt_message():
-        global encrypted_message
-        global key_input
-
-        if not encrypted_message or not key_input:
-            messagebox.showerror("Hata", "Şifreli mesaj ve anahtar gerekli!")
-            return
-
+    if len(title_message) == 0 or len(key_message) == 0 or len(secret_message)==0:
+        messagebox.showinfo(title="Error!",message="Please enter all info.")
+    else:
+        message_encrypted=encode(key_message,secret_message)
         try:
-            key_bytes = key_input.encode()
-            key_bytes = key_bytes.ljust(32)[:32]
-            key = base64.urlsafe_b64encode(key_bytes)
-
-            fernet = Fernet(key)
-            decrypted_message = fernet.decrypt(encrypted_message.encode()).decode()
-
-            messagebox.showinfo("Çözülen Mesaj", decrypted_message)
+            with open("mysecret.txt", "a") as data_file:
+                data_file.write(f'\n{title_message}\n{message_encrypted}')
         except:
-            messagebox.showerror("Hata", "Geçersiz anahtar veya şifreli mesaj!")
+            with open("mysecret.txt", "w") as data_file:
+                data_file.write(f'\n{title_message}\n{message_encrypted}')
+        finally:
+            title_message_entry.delete(0, END)
+            key_message_entry.delete(0, END)
+            secret_message_text.delete("1,0",END)
 
 
+def decrypt_notes():
+    message_encrypted = secret_message_text.get("1.0", END)
+    master_secret = key_message_entry.get()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if len(message_encrypted) == 0 or len(master_secret) == 0:
+        messagebox.showinfo(title="Error!", message="Please enter all information.")
+    else:
+        try:
+            decrypted_message = decode(master_secret,message_encrypted)
+            secret_message_text.delete("1.0", END)
+            secret_message_text.insert("1.0", decrypted_message)
+        except:
+            messagebox.showinfo(title="Error!", message="Please make sure of encrypted info.")
 
 
 
@@ -112,13 +95,8 @@ key_message_entry = tkinter.Entry(window)
 key_message_entry.pack()
 save_encrypted_message_button = tkinter.Button(text="Save & Encrypt",command=encrypt_message_create_desktop)
 save_encrypted_message_button.pack()
-decrypt_button = tkinter.Button(text="Decrypt",width=12,height=0,command=decrypt_message)
+decrypt_button = tkinter.Button(text="Decrypt",width=12,height=0,command=decrypt_notes)
 decrypt_button.pack()
-
-
-
-
-
 
 
 
